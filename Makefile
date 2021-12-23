df8429dcecbd016ec7965f10bf2199827661bf62
luca barendse <luca@dev.go2people.nl>
2021-12-23 10:21:31 +0100

    init

--- Makefile
+++
@@ -1,8 +0,0 @@
-npm_deps:
-	yarn install
-
-build: npm_deps
-	./node_modules/.bin/webpack -p --config webpack.prod.js
-
-dev: npm_deps
-	./node_modules/.bin/webpack -w --config webpack.dev.js
--- webpack.dev.js
+++
@@ -1,7 +0,0 @@
-const merge = require('webpack-merge');
-const common = require('./webpack.common.js');
-
-module.exports = merge(common, {
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
-});

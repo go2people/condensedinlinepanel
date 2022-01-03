42f8f8cd7f83bb46915d31277dcb17e53118924a
luca barendse <luca@dev.go2people.nl>
2022-01-03 16:22:39 +0100

    changed setup

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='wagtail-condensedinlinepanel',
+    name='condensedinlinepanel',
     version=__version__,
     description='',
     author='Karl Hobley',
